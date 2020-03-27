c4cc4e6e61b4c2629b4d9669806480b54cb9577f
Josh Blair <josh.blair@gmail.com>
2020-03-27 17:58:04 -0600

    added initial cut of pantry finder UI

--- src/app/pantry-locator/pantry-locator.component.ts
+++ src/app/pantry-locator/pantry-locator.component.ts
@@ -8,8 +8,39 @@ import { Component, OnInit } from '@angular/core';
 export class PantryLocatorComponent implements OnInit {
 
   constructor() { }
+  // constructor(private elementRef: ElementRef) { }
+  // constructor(private el: ElementRef, private renderer: Renderer) { }
 
   ngOnInit() {
   }
 
+  foodBanks = FOODBANKS;
+
+  // ngAfterViewInit(){
+  //   // this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor = 'red';
+  //   this.renderer.setElementStyle(this.el.nativeElement, 'background-color', '#5789D8');
+  //   this.renderer.setElementStyle(this.el.nativeElement, 'color', '#FFFFFF');
+  // }
+
+
+
 }
+
+export interface FoodBank {
+  name: string;
+  address: string;
+  notes: string
+}
+
+export const FOODBANKS: FoodBank[] = [
+  {
+    name: 'Hunger Free Colorado',
+    address: '1355 S Colorado Blvd 201, Denver, CO 80222',
+    notes: 'Accepts Donations'
+  },
+  {
+    name: 'Food Bank of the Rockies',
+    address: '3900 Nome St Warehouse, Denver, CO 80239',
+    notes: 'Accepts Donations'
+  }
+]
