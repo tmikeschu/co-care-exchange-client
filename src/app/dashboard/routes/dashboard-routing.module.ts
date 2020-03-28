5539f41cac7b9f25e9694b139442f1f8e48c9da1
dougpool <dnehring@gmail.com>
2020-03-27 18:28:36 -0600

    initial commit :tada:

--- src/app/dashboard/routes/dashboard-routing.module.ts
+++ src/app/dashboard/routes/dashboard-routing.module.ts
@@ -1,11 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from '../components/dashboard.component';
+import {PromptComponent} from '../components/prompt/prompt.component';
+import {SummaryComponent} from '../components/prompt/summary/summary.component';
+import {MetricsComponent} from '../components/metrics/metrics.component';
 
 const routes: Routes = [
+  // {
+  //   path: '',
+  //   component: DashboardComponent,
+  // },
   {
-    path: '',
-    component: DashboardComponent,
+    path: 'prompt',
+    component: PromptComponent,
+  },
+  {
+    path: 'summary',
+    component: SummaryComponent,
+  },
+  // {
+  //   path: 'dashboard',
+  //   component: DashboardComponent
+  // },
+  {
+    path: 'metrics',
+    component: MetricsComponent,
   },
 ];
 
