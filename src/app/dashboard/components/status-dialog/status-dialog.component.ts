7318682b4ba389adfa51fcf1a397cf54c86b7c14
John <johnmcguin88@gmail.com>
2020-03-30 14:58:55 -0600

    some formatting and lint fixes. unsure about the equality operators user here

--- src/app/dashboard/components/status-dialog/status-dialog.component.ts
+++ src/app/dashboard/components/status-dialog/status-dialog.component.ts
@@ -3,59 +3,58 @@ import { Component, Inject } from '@angular/core';
 import { Status } from '../models/dasboard';
 
 @Component({
-    selector: 'app-status-dialog',
-    templateUrl: 'status-dialog.component.html',
-    styleUrls: ['./status-dialog.component.scss']
-  })
-  export class StatusDialogComponent {
-    message: string;
-    sharemessage: string = 'Are you able to pick up {0} from {1}?';
-    needmessage: string = 'You matched as a sharer for {0} {1} of {2}.  Can you drop the items off at this location?  ';
-    confirmmessage: string = 'Did you pickup {0} from {1}? ';
-    okButtonText: string = 'OK';
-    showNoButton: boolean = false;
-    showMap: boolean = false;
-    showCancelButton: boolean = false;
-  
-    constructor(public dialogRef: MatDialogRef<StatusDialogComponent>, @Inject(MAT_DIALOG_DATA) public data: Status) {
-      console.log('StatusDialogComponent', data)
-      this.message = data.dialogmessage;
-      this.showMap = false;
-      this.showNoButton = false;
-      this.showCancelButton = false;
+  selector: 'app-status-dialog',
+  templateUrl: 'status-dialog.component.html',
+  styleUrls: ['./status-dialog.component.scss']
+})
+export class StatusDialogComponent {
+  message: string;
+  sharemessage = 'Are you able to pick up {0} from {1}?';
+  needmessage = 'You matched as a sharer for {0} {1} of {2}.  Can you drop the items off at this location?  ';
+  confirmmessage = 'Did you pickup {0} from {1}? ';
+  okButtonText = 'OK';
+  showNoButton = false;
+  showMap = false;
+  showCancelButton = false;
 
-      if(data.statusTypeId == 1 && data.statusId == 2){
-        this.okButtonText = 'Yes';
-        this.showNoButton = true;
-      }else{
-        this.okButtonText = 'OK';        
-      }
+  constructor(public dialogRef: MatDialogRef<StatusDialogComponent>, @Inject(MAT_DIALOG_DATA) public data: Status) {
+    console.log('StatusDialogComponent', data)
+    this.message = data.dialogmessage;
+    this.showMap = false;
+    this.showNoButton = false;
+    this.showCancelButton = false;
 
-      if(data.statusTypeId == 2){
-        this.showMap = true;
-        this.okButtonText = 'Yes';        
-      }    
-      
-      if(data.statusTypeId == 1 && data.statusId < 2){
-        this.showCancelButton = true;
-      }
-
-      if(data.statusTypeId == 2 && data.statusId < 3){
-        this.showCancelButton = true;
-      }
-    }
-  
-    confirm(): void {      
-      this.dialogRef.close(true);
+    if (data.statusTypeId == 1 && data.statusId == 2) {
+      this.okButtonText = 'Yes';
+      this.showNoButton = true;
+    } else {
+      this.okButtonText = 'OK';
     }
 
-    no(){
-      this.dialogRef.close(false);
+    if (data.statusTypeId == 2) {
+      this.showMap = true;
+      this.okButtonText = 'Yes';
     }
 
-    cancel(){      
-      this.dialogRef.close(false);
+    if (data.statusTypeId == 1 && data.statusId < 2) {
+      this.showCancelButton = true;
     }
-  
+
+    if (data.statusTypeId == 2 && data.statusId < 3) {
+      this.showCancelButton = true;
+    }
   }
-  
+
+  confirm(): void {
+    this.dialogRef.close(true);
+  }
+
+  no() {
+    this.dialogRef.close(false);
+  }
+
+  cancel() {
+    this.dialogRef.close(false);
+  }
+
+}
