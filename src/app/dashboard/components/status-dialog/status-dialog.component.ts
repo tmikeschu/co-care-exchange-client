d6af4ce1931e7a991b0e6fe2c9edf6c7836b137b
Josh Blair <josh.blair@gmail.com>
2020-03-30 18:31:40 -0600

    changed to use the agreement class and formatting

--- src/app/dashboard/components/status-dialog/status-dialog.component.ts
+++ src/app/dashboard/components/status-dialog/status-dialog.component.ts
@@ -1,6 +1,6 @@
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
 import { Component, Inject } from '@angular/core';
-import { Status } from '../models/dasboard';
+import { Agreement } from '../models/agreement';
 
 @Component({
   selector: 'app-status-dialog',
@@ -9,17 +9,17 @@ import { Status } from '../models/dasboard';
 })
 export class StatusDialogComponent {
   message: string;
-  sharemessage = 'Are you able to pick up {0} from {1}?';
-  needmessage = 'You matched as a sharer for {0} {1} of {2}.  Can you drop the items off at this location?  ';
-  confirmmessage = 'Did you pickup {0} from {1}? ';
-  okButtonText = 'OK';
-  showNoButton = false;
-  showMap = false;
-  showCancelButton = false;
+  sharemessage: string = 'Are you able to pick up {0} from {1}?';
+  needmessage: string = 'You matched as a sharer for {0} {1} of {2}.  Can you drop the items off at this location?  ';
+  confirmmessage: string = 'Did you pickup {0} from {1}? ';
+  okButtonText: string = 'OK';
+  showNoButton: boolean = false;
+  showMap: boolean = false;
+  showCancelButton: boolean = false;
 
-  constructor(public dialogRef: MatDialogRef<StatusDialogComponent>, @Inject(MAT_DIALOG_DATA) public data: Status) {
+  constructor(public dialogRef: MatDialogRef<StatusDialogComponent>, @Inject(MAT_DIALOG_DATA) public data: Agreement) {
     console.log('StatusDialogComponent', data)
-    this.message = data.dialogmessage;
+    this.message = data.dialogMessage;
     this.showMap = false;
     this.showNoButton = false;
     this.showCancelButton = false;
