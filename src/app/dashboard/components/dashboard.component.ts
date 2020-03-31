eec4558b94682a1281d07861409481576fec691d
Josh Blair <josh.blair@gmail.com>
2020-03-30 18:29:08 -0600

    refactored dashboard code behind to march the changes in dashboard service

--- src/app/dashboard/components/dashboard.component.ts
+++ src/app/dashboard/components/dashboard.component.ts
@@ -1,11 +1,12 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialog, MatDialogRef} from '@angular/material';
 import { StatusDialogComponent } from './status-dialog/status-dialog.component';
-import { Status } from './models/dasboard';
+// import { Status } from './models/dasboard';
 import { DashboardService } from 'src/app/services/cce/dashboard.service';
 import { Observable, Subscription } from 'rxjs';
 import { SiteFooterComponent } from 'src/app/shared/site-footer/site-footer.component';
 import { map } from 'rxjs/operators';
+import { Agreement } from './models/agreement';
 
 @Component({
   selector: 'app-cce-home',
@@ -13,31 +14,28 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-  localrequests: Status[];
-  subscription: Subscription;
+  // localrequests: Status[];
+  subscriptionNeeds: Subscription;
+  subscriptionShares: Subscription;
   dashboardservice: DashboardService;
   footer: SiteFooterComponent;
+  agreementNeeds: any[];
+  agreementShares: any[];
   
   constructor(public dialog: MatDialog, dashboardservice: DashboardService, footer: SiteFooterComponent) {     
     this.dashboardservice = dashboardservice;
-    this.subscription = this.dashboardservice.agreements.subscribe();
+    this.subscriptionNeeds = this.dashboardservice.agreementNeeds.subscribe();
+    this.subscriptionShares = this.dashboardservice.agreementShares.subscribe();
   }
 
   ngOnInit() {
-    window.scrollTo(0, 0);   
+    window.scrollTo(0, 0); 
   }
 
-  test(){      
-    //this.dashboardservice.getRequests2();
-    this.dashboardservice.testgraphQL().subscribe(data => {
-      console.log('testgraphQL', data);
-    });
-  }
-
-  handleStatusClick(status: Status) {
+  handleStatusClick(agreement: Agreement) {
     const dialogRef = this.dialog.open(StatusDialogComponent, {
       width: '300px',
-      data: status
+      data: agreement
     });
 
     dialogRef.afterClosed().subscribe(confirm => { 
@@ -53,7 +51,8 @@ export class DashboardComponent implements OnInit {
 
   ngOnDestroy() {
     // unsubscribe to ensure no memory leaks
-    this.subscription.unsubscribe();
+    this.subscriptionNeeds.unsubscribe();
+    this.subscriptionShares.unsubscribe();
   }
 }
 
