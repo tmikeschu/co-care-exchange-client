1ab2cc5e5a0c59f646f59fff3d19f611981f0a5b
Josh Blair <josh.blair@gmail.com>
2020-03-30 18:27:52 -0600

    refactored status class into dashboard result to match the resulting object more closely

--- src/app/dashboard/components/models/dasboard.ts
+++ src/app/dashboard/components/models/dasboard.ts
@@ -1,18 +1,10 @@
-export class Status {
-    requestId: string;
-    name: string;
-    quantity: number;
-    itemType: string;
-    status: string;
-    sourceName: string;
-    address: string;
-    statusId: number;
-    styleclass?: string;
-    confirm?: boolean;
-    deny?: boolean;
-    statusType: string;
-    statusTypeId: number;
-    dialogmessage: string;
-    lat: string;
-    long: string;
+import { Agreement } from './agreement';
+
+export class Result {
+    data: any = {
+        dashboard: {
+            requested: [Agreement],
+            shared: [Agreement]
+        }
+    }
 }
