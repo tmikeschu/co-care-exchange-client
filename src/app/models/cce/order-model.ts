ef5b1be8bcbc4c25e8cb5e6faeeb6ddbd65faa8c
Josh Blair <josh.blair@gmail.com>
2020-04-01 15:40:49 -0600

    updated order change status model

--- src/app/models/cce/order-model.ts
+++ src/app/models/cce/order-model.ts
@@ -1,13 +1,16 @@
-export interface OrderCancelModel {
-    orderId: string;
-    cancellingUserId: string;
-    reason: string;
+// export interface OrderCancelModel {
+//     orderId: string;
+//     cancellingUserId: string;
+//     reason: string;
+//     clientMutationId: string;
+// }
+
+export interface OrderStatusChangeModel {
+    id: string;
+    requesterUserId: string;
+    status: number;
+    cancellationReason: string;
     clientMutationId: string;
 }
 
-export interface OrderChangeStatusModel {
-    orderId: string;
-    agreementUserId: string;
-    newStatusId: number;
-    clientMutationId: string;
-}
+//id, status, cancellationReason, requesterUserId
