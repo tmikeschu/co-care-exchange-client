00934c913f170474665d8dfa026212cf5eec5904
Josh Blair <josh.blair@gmail.com>
2020-04-07 17:16:21 -0600

    fixed query, agreementId changed to orderId; cleanup

--- src/app/core/services/cce/dashboard.service.ts
+++ src/app/core/services/cce/dashboard.service.ts
@@ -38,142 +38,6 @@ export class DashboardService {
     this.getDashboard().subscribe(result => {
       console.log('dashboard results:', result);
 
-      // if (!result.data.dashboard.requested.length) {
-
-      //   // TODO: remove once graphql API returns data
-      //   result =
-      //   {
-      //     "data": {
-      //       "dashboard": {
-      //         "requested": [
-      //           {
-      //             "name": "Diapers",
-      //             "statusId": 0,
-      //             "statusText": "Looking for Supplier",
-      //             "agreementId": "41d18d0501c14cc89e190667c45f3c57",
-      //             "dialogMessage": "A supplier of your request has not yet been found.",
-      //             "deliveryCoordinates": {
-      //               "latitude": 39.7481,
-      //               "longitude": -104.98742
-      //             },
-      //             "deliveryAddress": null
-      //           },
-      //           {
-      //             "name": "Tampons",
-      //             "statusId": 1,
-      //             "statusText": "New Match!",
-      //             "agreementId": "1d9e1eeffbac40db94e062ac00718e12",
-      //             "dialogMessage": "A supplier has been found. Awaiting their confirmation.",
-      //             "deliveryCoordinates": {
-      //               "latitude": 39.7481,
-      //               "longitude": -104.98742
-      //             },
-      //             "deliveryAddress": null
-      //           },
-      //           {
-      //             "name": "Adult Meal(s)",
-      //             "statusId": 2,
-      //             "statusText": "Delivery Pending",
-      //             "agreementId": "1d1e1eeffbac40db94e062ac00718e12",
-      //             "dialogMessage": "Supplier has confirmed your request and sent it out for delivery. Have you received this item?",
-      //             "deliveryCoordinates": {
-      //               "latitude": 39.7481,
-      //               "longitude": -104.98742
-      //             },
-      //             "deliveryAddress": null
-      //           },
-      //           {
-      //             "name": "Child Meal(s)",
-      //             "statusId": 3,
-      //             "statusText": "Fulfilled",
-      //             "agreementId": "1d1e1eeffbac40db94e062ac00718e02",
-      //             "dialogMessage": "Your request has been delivered and is complete.",
-      //             "deliveryCoordinates": {
-      //               "latitude": 39.7481,
-      //               "longitude": -104.98742
-      //             },
-      //             "deliveryAddress": null
-      //           },
-      //           {
-      //             "name": "Something",
-      //             "statusId": 4,
-      //             "statusText": "Cancelled",
-      //             "agreementId": "1d1e10effbac40db94e062ac00718e02",
-      //             "dialogMessage": "Your request has been cancelled.",
-      //             "deliveryCoordinates": {
-      //               "latitude": 39.7481,
-      //               "longitude": -104.98742
-      //             },
-      //             "deliveryAddress": null
-      //           }
-      //         ],
-      //         "shared": [
-      //           {
-      //             "name": "Diapers",
-      //             "statusId": 0,
-      //             "statusText": "Looking for match",
-      //             "agreementId": "41d18a0501c14cc89e190667c45f3c57",
-      //             "dialogMessage": "No requests yet.",
-      //             "deliveryCoordinates": {
-      //               "latitude": 39.7481,
-      //               "longitude": -104.98742
-      //             },
-      //             "deliveryAddress": "1999 N Broadway Denver CO"
-      //           },
-      //           {
-      //             "name": "Tampons",
-      //             "statusId": 1,
-      //             "statusText": "New Match!",
-      //             "agreementId": "0d9e1eeffbac40db94e062ac00718e12",
-      //             "dialogMessage": "Someone is in need at the following address. Are you still able to supply and deliver them?",
-      //             "deliveryCoordinates": {
-      //               "latitude": 39.7481,
-      //               "longitude": -104.98742
-      //             },
-      //             "deliveryAddress": "1999 N Broadway Denver CO"
-      //           },
-      //           {
-      //             "name": "Adult Meal(s)",
-      //             "statusId": 2,
-      //             "statusText": "Delivery Pending",
-      //             "agreementId": "1b1e1eeffbac40db94e062ac00718e12",
-      //             "dialogMessage": "You have sent items out for delivery to this address.",
-      //             "deliveryCoordinates": {
-      //               "latitude": 39.7481,
-      //               "longitude": -104.98742
-      //             },
-      //             "deliveryAddress": "1999 N Broadway Denver CO"
-      //           },
-      //           {
-      //             "name": "Child Meal(s)",
-      //             "statusId": 3,
-      //             "statusText": "Fulfilled",
-      //             "agreementId": "1d6e1eeffbac40db94e062ac00718e02",
-      //             "dialogMessage": "Receiver has confirmed delivery.  This order is completed.",
-      //             "deliveryCoordinates": {
-      //               "latitude": 39.7481,
-      //               "longitude": -104.98742
-      //             },
-      //             "deliveryAddress": "1999 N Broadway Denver CO"
-      //           },
-      //           {
-      //             "name": "Something",
-      //             "statusId": 4,
-      //             "statusText": "Cancelled",
-      //             "agreementId": "1d1e10effbac90db94e062ac00718e02",
-      //             "dialogMessage": "This request has been cancelled.",
-      //             "deliveryCoordinates": {
-      //               "latitude": 39.7481,
-      //               "longitude": -104.98742
-      //             },
-      //             "deliveryAddress": "1999 N Broadway Denver CO"
-      //           }
-      //         ]
-      //       }
-      //     }
-      //   }
-      // };
-
       this.hasNeeds = result.data.dashboard.requested.length;
       this.hasShares = result.data.dashboard.shared.length
 
@@ -189,7 +53,29 @@ export class DashboardService {
 
   getDashboard(): Observable<Result> {
     const query = {
-      'query': 'query View ($userId:ID!){ dashboard(userId:$userId) {requested{name, statusText, agreementId, dialogMessage, statusId, deliveryAddress, requestId, shareId}, shared{name, statusText, agreementId, dialogMessage, statusId, deliveryAddress, shareId, requestId}}}',
+      'query': `query View($userId: ID!) {
+        dashboard(userId: $userId) {
+            requested {
+                name,
+                statusText,
+                orderId,
+                dialogMessage,
+                statusId,
+                deliveryAddress,
+                requestId,
+                shareId
+            }, shared {
+                name,
+                statusText,
+                orderId,
+                dialogMessage,
+                statusId,
+                deliveryAddress,
+                shareId,
+                requestId
+            }
+        }
+    }`,
       'variables': {
         "userId": this.userProfile && this.userProfile.id ? this.userProfile.id : '22201103dec0466fb44f1926bc1687c1' // TODO: just in case no items return for the demo
       }
