3b0c3a5b071f7292477a745d3b2ba67b0f6b4cc6
Josh Blair <josh.blair@gmail.com>
2020-04-14 20:07:59 -0600

    added markup, css, and controller logic for contributors component

--- src/app/contributors/contributors.component.ts
+++ src/app/contributors/contributors.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+// import { MatDialogRef } from '@angular/material';
 
 @Component({
   selector: 'app-contributors',
@@ -8,6 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class ContributorsComponent implements OnInit {
 
   constructor() { }
+  // constructor(public dialogRef: MatDialogRef<ContributorsComponent>) { }
 
   ngOnInit() {
   }
