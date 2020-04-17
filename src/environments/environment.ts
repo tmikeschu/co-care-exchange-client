931e241cde3a08603d89086aa90d980d2d87faae
jasonbrandt42 <jason.brandt@hdslabs.com>
2020-04-17 10:06:55 -0600

    update link

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,7 @@ import { awsEnv } from './awsEnv.qa';
 export const environment = {
   ...commonEnv,
   ...awsEnv,
-  serverUrl: 'https://api.cocareexchange.org/graphql?',
+  serverUrl: 'https://dev-api.cocareexchange.org/graphql',
   apiKey: 'zPrRE3Fjmz5mZ1sFQxoRY1qtGRxTM5f41Wu4GdiU',
   production: false,
 };
