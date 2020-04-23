da2f66023d3403d9c526bddff321ee96af509f10
John <johnmcguin88@gmail.com>
2020-04-23 14:42:02 -0600

    add request notes to return payload for debuggin

--- src/app/core/services/cce/prompt.service.ts
+++ src/app/core/services/cce/prompt.service.ts
@@ -43,7 +43,8 @@ export class PromptService {
           answer {
             id,
             promptId,
-            numberValue
+            numberValue,
+            requestNotes
           },
           clientMutationId
         }
