b9bc85d6f6c3990a50b0ecef289908432676f75a
John <johnmcguin88@gmail.com>
2020-04-29 06:09:27 -0600

    get rid of selectedAgreement logic in dashboard component

--- src/app/dashboard/components/dashboard.component.ts
+++ src/app/dashboard/components/dashboard.component.ts
@@ -1,10 +1,8 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Router } from '@angular/router';
 import { MatDialog } from '@angular/material';
 import { Observable } from 'rxjs';
 
 import { DashboardService, IDashboardState } from 'src/app/core/services/cce/dashboard.service';
-import { Agreement } from './models/agreement';
 import { Status } from 'src/app/core/constants/enums';
 
 
@@ -19,18 +17,12 @@ export class DashboardComponent implements OnInit, OnDestroy {
 
   constructor(
     public dialog: MatDialog,
-    private dashboardService: DashboardService,
-    private router: Router,
+    private dashboardService: DashboardService
   ) {}
 
   ngOnInit() {
     this.dashboardService.startPolling();
     this.vm$ = this.dashboardService.state$;
-  }
-
-  handleStatusClick(agreement: Agreement, type: String) {
-    this.dashboardService.setSelectedAgreement(agreement);
-    this.router.navigate(['/agreement-detail'], { queryParams: { type }});
   }
 
   getStyle(status: Status): string {
