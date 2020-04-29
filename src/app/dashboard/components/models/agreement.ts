d89efa2bba5b5dca0028cbcdd65a0124abd1798f
John <johnmcguin88@gmail.com>
2020-04-29 17:18:38 -0600

    remove deprecated requesterNotes and sharerNotes from agreement model

--- src/app/dashboard/components/models/agreement.ts
+++ src/app/dashboard/components/models/agreement.ts
@@ -17,8 +17,6 @@ export class Agreement {
     shareId: string;
     requestId: string;
     unitOfIssue: String;
-    sharerNotes: string;
-    requesterNotes: string;
     details: String;
     addressLabel: String;
     requesterName: string;
