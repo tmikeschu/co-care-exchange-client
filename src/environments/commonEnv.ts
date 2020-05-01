ede4f63d94e5d046208c88af3cd132f7ad094093
Josh Blair <josh.blair@gmail.com>
2020-05-01 14:50:47 -0600

    set password policy maxLength to 30

--- src/environments/commonEnv.ts
+++ src/environments/commonEnv.ts
@@ -1,6 +1,7 @@
 export const commonEnv = {
     passwordPolicy: {
         minLength: 8,
+        maxLength: 30,
         requireNumber: true,
         requireSpecialCharacter: true,
         requireUppercaseLetters: true,
