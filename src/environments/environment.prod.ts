2e2fbae5660db77ea94b47deef7e0bb1fceddcdc
John <johnmcguin88@gmail.com>
2020-05-15 15:31:29 -0600

    WARNING: update env to production for PWA test. Make sure not to merge this into production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,7 +5,9 @@ export const environment = {
   ...commonEnv,
   ...awsEnv,
   // the real prod envs should go here
-  serverUrl: 'https://api.cocareexchange.org/graphql',
-  apiKey: 'ClCInL23rFaCf5pbrQc028dkXSUur8NMavQedzur',
+  serverUrl: 'https://dev-api.cocareexchange.org/graphql',
+  apiKey: 'zPrRE3Fjmz5mZ1sFQxoRY1qtGRxTM5f41Wu4GdiU',
+  // serverUrl: 'https://api.cocareexchange.org/graphql',
+  // apiKey: 'ClCInL23rFaCf5pbrQc028dkXSUur8NMavQedzur',
   production: true,
 };
