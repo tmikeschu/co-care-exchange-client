db9db593ce93332e81eac4f26ba3ff74f49bb52c
John <johnmcguin88@gmail.com>
2020-05-15 15:27:54 -0600

    fallback service worker load

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ platformBrowserDynamic()
   .bootstrapModule(AppModule)
   .then(() => {
     if ('serviceWorker' in navigator && environment.production) {
-      navigator.serviceWorker.register('ngsw-worker.js');
+      navigator.serviceWorker.register('./ngsw-worker.js');
     }
   })
   .catch(err => console.error(err));
