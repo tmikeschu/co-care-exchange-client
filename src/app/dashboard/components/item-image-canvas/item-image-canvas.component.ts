fc7d02d7d01630e89875181b24d50653b930ca17
Mike Schutte <tmikeschutte@gmail.com>
2020-06-11 11:16:29 -0400

    if else on image note

--- src/app/dashboard/components/item-image-canvas/item-image-canvas.component.ts
+++ src/app/dashboard/components/item-image-canvas/item-image-canvas.component.ts
@@ -23,18 +23,19 @@ export class ItemImageCanvasComponent implements OnInit {
 
   loadPhoto() {
     Storage.get(this.imageUrl, { download: true, level: 'public' })
-      .then(res => {
+      .then((res) => {
         const image = JSON.parse(JSON.stringify(res))['Body'];
         this.imgSrc = image;
         this.viewState = 'loaded';
       })
-      .catch(err => {
+      .catch((err) => {
         console.log('image canvas error => ', err);
         this.viewState = 'error';
       })
       .finally(() => {
         // for some reason these state updates are outside the "angular zone",
-        // so we use the change detector to signify updates
+        // so we use the change detector to signify updates.
+        // Might be due to ChangeDetectionStrategy.OnPush higher up in the tree.
         this.cd.detectChanges();
       });
   }
