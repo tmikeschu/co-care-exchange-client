234e231f7d2e93c0a38f2d53c59f7ceef369e607
John <johnmcguin88@gmail.com>
2020-06-15 16:11:49 -0600

    update error log text only

--- src/app/graphql/utils/error-handler.ts
+++ src/app/graphql/utils/error-handler.ts
@@ -3,7 +3,7 @@ import { throwError } from 'rxjs';
 export function handleGQLErrors(data: any) {
     if (data && data.errors) {
         const messages = data.errors.map((e) => e.message).join(', ');
-        console.error('A GraphQL error occurred: ', messages);
+        console.log('[GraphQL] errors: ', data.errors);
         return throwError(messages);
     }
     return data.data;
