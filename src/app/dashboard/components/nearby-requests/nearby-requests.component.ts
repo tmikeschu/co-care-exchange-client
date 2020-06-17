a04533b1142d26c49b8c918febd14b7d45f36b40
John <johnmcguin88@gmail.com>
2020-06-16 18:46:11 -0600

    setting up inputs to the queries

--- src/app/dashboard/components/nearby-requests/nearby-requests.component.ts
+++ src/app/dashboard/components/nearby-requests/nearby-requests.component.ts
@@ -1,36 +1,45 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable, Subject, combineLatest, of, concat } from 'rxjs';
 import { map, switchMap, startWith, filter } from 'rxjs/operators';
 import { Agreement } from '../models/agreement';
 import { NearbyRequestsGQL } from 'src/app/graphql/generatedSDK';
 import { AuthenticationService } from 'src/app/core/services/cce/authentication.service';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-nearby-requests',
   templateUrl: './nearby-requests.component.html',
   styleUrls: ['./nearby-requests.component.scss', '../dashboard.component.scss', '../nearby-items/nearby-items.component.scss']
 })
-export class NearbyRequestsComponent implements OnInit {
+export class NearbyRequestsComponent implements OnInit, OnDestroy {
   vm$: Observable<{ state: string, requests: Array<Agreement> }>;
   user$: Observable<any>;
+  filter = new FormControl('organization');
+  filter$: Observable<string>;
+  inputs$: Observable<any>;
+  destroy$ = new Subject();
 
   constructor(
     private authSvc: AuthenticationService,
     private nearbyRequestsQuery: NearbyRequestsGQL) {
+  }
+
+  ngOnInit() {
     this.user$ = this.authSvc.auth$.pipe(
       filter((authState) => authState.user && authState.user.userProfile),
       map((authState) => authState.user.userProfile)
     );
-  }
 
-  ngOnInit() {
-    this.vm$ = this.user$.pipe(
-      switchMap((user: any) => this.getNearbyRequests(user.id)),
+    this.filter$ = concat(of('organization'), this.filter.valueChanges);
+    this.inputs$ = combineLatest([this.filter$, this.user$]);
+    this.vm$ = this.inputs$.pipe(
+      switchMap(([filterVal, user]) => this.getNearbyRequests(user.id, filterVal)),
       startWith({ state: 'loading', requests: [] })
     );
   }
 
-  getNearbyRequests(userId: string) {
+  getNearbyRequests(userId: string, filterVal: string) {
+    // TODO: pass filter val once API support is there.
     return this.nearbyRequestsQuery.watch({ userId })
       .valueChanges
       .pipe(
@@ -43,5 +52,14 @@ export class NearbyRequestsComponent implements OnInit {
           };
         })
       );
+  }
+
+  formatItemDetails(agreement: Agreement) {
+    return `${agreement.quantity}${agreement.unitOfIssue ? ', ' + agreement.unitOfIssue : ''}${agreement.details ? ', ' + agreement.details : ''}`
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
