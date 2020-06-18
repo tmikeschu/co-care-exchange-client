f8d12e8b9c28fa5aa2ff6cd5f365a3b6abf9f07c
jasonbrandt42 <jason.brandt@hdslabs.com>
2020-06-18 10:51:02 -0600

    fix for satus name

--- src/app/dashboard/components/dashboard.component.ts
+++ src/app/dashboard/components/dashboard.component.ts
@@ -91,7 +91,7 @@ export class DashboardComponent implements OnInit, OnDestroy {
         // Matched
         return 'contentstatusgreen';
       }
-      case Status.DeliveryPending: {
+      case Status.OrderConfirmed: {
         // Confirmed
         return 'contentstatusyellow';
       }
