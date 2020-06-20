c49307cecb4bed048afb7ad82387b8f7c0bf4a6f
Mike Schutte <tmikeschutte@gmail.com>
2020-06-20 09:56:03 -0400

    Prettier acceptance files not on master

--- src/app/dashboard/components/item-share/item-share.component.ts
+++ src/app/dashboard/components/item-share/item-share.component.ts
@@ -1,4 +1,15 @@
-import { Component, OnInit, OnDestroy, Input, Output, ChangeDetectionStrategy, EventEmitter, Renderer2, ElementRef, ViewChild } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  OnDestroy,
+  Input,
+  Output,
+  ChangeDetectionStrategy,
+  EventEmitter,
+  Renderer2,
+  ElementRef,
+  ViewChild,
+} from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Observable, Subject, BehaviorSubject, combineLatest } from 'rxjs';
 import { debounceTime, filter, distinctUntilChanged, takeUntil, take, tap, finalize } from 'rxjs/operators';
@@ -23,9 +34,9 @@ import { UserService } from 'src/app/core/services/user.service';
 export class ItemShareComponent implements OnInit, OnDestroy {
   @Input() vm: IItemDetailState;
   @Output() createNote = new EventEmitter<Pick<ICreateOrderNoteInput, 'noteBody' | 'itemId' | 'imageUrl'>>();
-  @Output() updateItem = new EventEmitter<{ orderUpdate: Agreement, updates: Partial<OrderChangeInput> }>();
+  @Output() updateItem = new EventEmitter<{ orderUpdate: Agreement; updates: Partial<OrderChangeInput> }>();
 
-  showImageArea:boolean = false;
+  showImageArea: boolean = false;
   imagename: string = '';
   userProfile: UserProfile;
   status = Status; // enum binding to use in view template
@@ -38,7 +49,7 @@ export class ItemShareComponent implements OnInit, OnDestroy {
   orderNoteFC: FormControl = new FormControl('');
   orderNoteFC$: Observable<string>;
 
-  constructor(private dialog: MatDialog, private router: Router, private userService: UserService) { }
+  constructor(private dialog: MatDialog, private router: Router, private userService: UserService) {}
 
   ngOnInit() {
     this.userProfile = this.userService.getCurrentUserProfile();
@@ -52,7 +63,7 @@ export class ItemShareComponent implements OnInit, OnDestroy {
         filter((val) => val && val !== ''),
         takeUntil(this.stop$)
       )
-      .subscribe((val) => (this.currentNoteVal = val));    
+      .subscribe((val) => (this.currentNoteVal = val));
   }
 
   ngOnDestroy() {
@@ -80,7 +91,7 @@ export class ItemShareComponent implements OnInit, OnDestroy {
   //       reason: 'Sharer confirmed the delivery of the items',
   //     },
   //   });
-  // } 
+  // }
 
   onConfirmFulfillemnt(agreement: Agreement) {
     this.updateItem.emit({
@@ -93,7 +104,7 @@ export class ItemShareComponent implements OnInit, OnDestroy {
     });
   }
 
-  onConfirmMatch(){    
+  onConfirmMatch() {
     this.updateItem.emit({
       orderUpdate: this.vm.itemDetails,
       updates: {
@@ -103,14 +114,14 @@ export class ItemShareComponent implements OnInit, OnDestroy {
     });
   }
 
-  onSubmitEdit() {   
+  onSubmitEdit() {
     this.createNote.emit({ noteBody: this.currentNoteVal, itemId: this.vm.itemDetails.itemId, imageUrl: null });
     this.orderNoteFC.patchValue('');
   }
-  
+
   navigateBackToDashboard() {
     //if (!this.modalVisible) {
-      this.router.navigate(['/dashboard']);
+    this.router.navigate(['/dashboard']);
     //} else {
     //  return false;
     //}
@@ -122,21 +133,23 @@ export class ItemShareComponent implements OnInit, OnDestroy {
     }`;
   }
 
-  takepicture(){    
+  takepicture() {
     console.log('vm', this.vm);
     this.imagename = Date.now().toString();
     this.showImageArea = true;
-  }  
+  }
 
-  hidepicture(){
+  hidepicture() {
     this.showImageArea = false;
   }
 
-  pictureTaken(){
-    this.showImageArea = false;    
-    this.createNote.emit({ noteBody: 'image', itemId: this.vm.itemDetails.itemId, imageUrl: this.userProfile.id + '/' + this.vm.itemDetails.shareId + '/' + this.imagename });
+  pictureTaken() {
+    this.showImageArea = false;
+    this.createNote.emit({
+      noteBody: 'image',
+      itemId: this.vm.itemDetails.itemId,
+      imageUrl: this.userProfile.id + '/' + this.vm.itemDetails.shareId + '/' + this.imagename,
+    });
     this.orderNoteFC.patchValue('');
   }
-
-  
 }
